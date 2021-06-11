65d4b896b32094cceab49ddcd9e6f4c40684ebbc
arabelne <nelson.arabel@lamercantil.com.ar>
2021-06-11 08:13:23 -0300

    Agregado de Entity Producto

---
+++ src/main/java/com/narabel/servicioproductos/models/entity/Producto.java
@@ -0,0 +1,34 @@
+package com.narabel.servicioproductos.models.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+	private static final long serialVersionUID = 4204899169675334166L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private BigDecimal precio;
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private LocalDate createAt;
+
+}
